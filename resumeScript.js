83766cf59c756336d2d33c2391447f16c35162f9
tcano0505 <tommy.cano19@my.stjohns.edu>
2023-07-07 01:59:52 -0400

    Rough Draft of FrontEnd

---
+++ resumeScript.js
@@ -0,0 +1,7 @@
+document.getElementsById("btn-1").onclick = function() {
+    location.href = "./submission.html";
+};
+
+document.getElementsById("btn-2").onclick = function() {
+    location.href = "./keyword.html";
+};
