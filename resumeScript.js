a3413aceabc74c13f9f71b265dc3b1750f66d876
tcano0505 <tommy.cano19@my.stjohns.edu>
2023-07-09 13:22:50 -0400

    Submission Page is Ready

--- resumeScript.js
+++ resumeScript.js
@@ -1,7 +1,37 @@
-document.getElementsById("btn-1").onclick = function() {
-    location.href = "./submission.html";
-};
+window.addEventListener("load", ()=>{
+    const input= document.getElementById("upload");
+    const filewrapper = document.getElementById("filewrapper");
 
-document.getElementsById("btn-2").onclick = function() {
-    location.href = "./keyword.html";
-};
+    input.addEventListener("change", (e)=>{
+        let fileName = e.target.files[0].name;
+        let fileType = e.target.value.split(".").pop();
+        fileshow(fileName, fileType);
+    })
+
+    const fileshow = (fileName, fileType) =>{
+        const showFileboxElement = document.createElement("div");
+        showFileboxElement.classList.add("showFilebox");
+        const leftElement = document.createElement("div");
+        leftElement.classList.add("left");
+        const fileTypeElement = document.createElement("span");
+        fileTypeElement.classList.add("fileType");
+        fileTypeElement.innerHTML=fileType;
+        leftElement.append(fileTypeElement);
+        const fileTitleElement = document.createElement("h3");
+        fileTitleElement.innerHTML=fileName;
+        leftElement.append(fileTitleElement);
+        showFileboxElement.append(leftElement);
+        const rightElement = document.createElement("div");
+        rightElement.classList.add("right");
+        showFileboxElement.append(rightElement);
+        const crossElement = document.createElement("span");
+        crossElement.innerHTML="&#215;";
+        rightElement.append(crossElement);
+        filewrapper.append(showFileboxElement);
+
+        crossElement.addEventListener("click", ()=>{
+            filewrapper.removeChild(showFileboxElement);
+        })
+    }
+
+})
